4e37b848b8e0d933b352cd38d24a1bd8bfa73b5a
staceylaurel <64053039+staceylaurel@users.noreply.github.com>
2020-10-08 13:34:39 -0500

    Add files via upload
    1st 30min push 1:34pm server side

---
+++ src/server/config/index.ts
@@ -0,0 +1,16 @@
+import * as dotenv from 'dotenv';
+
+dotenv.config();
+
+export default{
+    mysql:{
+        host: process.env.DB_HOST,
+        user: process.env.DB_USER,
+        database: process.env.DB_DATABASE,
+        password: process.env.DB_PASSWORD,
+    
+    },
+    auth:{
+        secret: process.env.JWT_SECRET,
+    }
+}
